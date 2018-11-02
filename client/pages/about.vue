68772a635286ecd73d58267accbf0622c1457b03
alony <alony@wix.com>
2018-11-02 09:50:26 +0200

    bootstrap

---
+++ client/pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Us</h1>
+      <p>
+        We are two top level software engineers who are going to change the world as you know it.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ client/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hello world!</h1>
+    </div>
+  </section>
+</template>
