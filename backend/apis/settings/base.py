fd6cc7dca53198b61fa3deb4739d18c8f0f40489
Tzahi Furmanski <tzahi.fur@gmail.com>
2019-07-27 00:58:10 +0300

    Create a separate settings section

--- backend/apis/settings/base.py
+++ backend/apis/settings/base.py
@@ -31,9 +31,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'u=3l@@u_dg+kkphhtpx77+6jdb9w0arjc#tt4bcft85d2e!0f3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -126,11 +123,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
-#STATIC_URL = '/static/'
-
-# TODO: Create a settings subsection
-
-#DEBUG = False
---
+++ backend/apis/settings/development.py
@@ -0,0 +1,3 @@
+from apis.settings.base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
