cdaa4fa61d51b8aa0bccb973f08a2fcfb0184ff9
Tzahi Furmanski <tzahi.fur@gmail.com>
2019-08-02 11:54:14 +0300

    Add subscribers CR operations + partial UT

--- backend/apis/tests/base.py
+++ backend/apis/tests/base.py
@@ -1,9 +1,19 @@
 import pytest
 from django.test import Client
 
+from apis.models.subscriber import Subscriber
+
 
 @pytest.mark.django_db
 class BaseTest:
     @pytest.fixture()
     def client(self):
         return Client()
+
+    @pytest.fixture()
+    def subscriber(self):
+        return Subscriber.objects.create(first_name="David", last_name="Goggins", email="david@goggins.com")
+
+    @pytest.fixture()
+    def subscriber2(self):
+        return Subscriber.objects.create(first_name="Mark", last_name="Whalberg", email="mark@whalberg.com")
--- backend/apis/wsgi.py
+++ backend/apis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apis.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apis.settings.production')
 
 application = get_wsgi_application()
